a882abce08ea72768b448d978911e10477f70354
helmiazkia <azkiasekian@gmail.com>
2022-10-12 10:58:00 +0700

    pemweb

---
+++ minggu 5/proses.php
@@ -0,0 +1,10 @@
+<?php
+    print_r($_POST);
+    $fnim = $_POST['fnim'];
+    $fnam = $_POST['fnam'];
+    $ftgl = $_POST['ftgl'];
+    $Gender = $_POST['Gender'];
+    $fprod = $_POST['fprod'];
+    $fbid = $_POST['fbid'];
+?>
+
